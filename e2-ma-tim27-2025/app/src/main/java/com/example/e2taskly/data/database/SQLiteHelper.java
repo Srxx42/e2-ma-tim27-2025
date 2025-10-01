f8b53fb47aaa8858b81f6f0613254e37363fa0b2
lnosovic <lnosovic46@gmail.com>
2025-10-01 16:37:44 +0200

    Change to sqlite

--- e2-ma-tim27-2025/app/src/main/java/com/example/e2taskly/data/database/SQLiteHelper.java
+++ e2-ma-tim27-2025/app/src/main/java/com/example/e2taskly/data/database/SQLiteHelper.java
@@ -53,7 +53,7 @@ public class SQLiteHelper extends SQLiteOpenHelper {
                 "name TEXT NOT NULL, " +
                 "leader_id TEXT NOT NULL, " +
                 "member_ids TEXT, " +
-                "mission_started TEXT NOT NULL," +
+                "mission_status TEXT NOT NULL," +
                 "current_mission_id TEXT," +
                 "FOREIGN KEY(leader_id) REFERENCES " + T_USERS + "(id) ON DELETE CASCADE" +
                 ")");
